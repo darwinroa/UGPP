b21101367f290ce6b4ad2c93c133a6528c6ccb2b
Darwin Roa <darwinmichaelroamora@gmail.com>
2024-11-24 11:20:33 -0500

    feat(tramites): Agrega loop query para listar todos los trámtes disponibles, además contiene su buscador con ajax y un paginado

---
+++ utilities/mdw_html_search_input.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Retorna el HTML del campo de búsqueda
+ * Requiere un ID para el campo de búsqueda
+ */
+function mdw_html_search_field($atts)
+{
+  // Definir los atributos aceptados y sus valores predeterminados
+  $attributes = shortcode_atts(
+    array(
+      'post_type'  => 'post',
+      'placeholder'  => 'buscar',
+    ),
+    $atts
+  );
+
+  $postType = $attributes['post_type'];
+  $placeholder = $attributes['placeholder'];
+
+  return "
+    <form id='mdw-search-form-$postType' class='mdw__search_field'>
+      <input type='text' id='mdw-search-$postType' class='mdw__search_input' name='search' placeholder='$placeholder'>
+      <span class='mdw__search_icon'></span>
+    </form>
+    ";
+}
+
+add_shortcode('mdw_search', 'mdw_html_search_field');
